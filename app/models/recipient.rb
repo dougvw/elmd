ae097b81769c1cba22fef7702a9a46eda3d4a116
Doug VanderWoude <dougvw@gmail.com>
2010-02-17 20:59:43 -0500

    minor

--- app/models/recipient.rb
+++ app/models/recipient.rb
@@ -4,7 +4,7 @@ class Recipient < ActiveRecord::Base
   
   before_save :make_token
   
-  validates_presence_of :first_name, :last_name, :email
+  validates_presence_of :email, :zip
   
   def make_token
     self.token = Digest::MD5.hexdigest("#{self.email}-#{Time.now.to_i}")
--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,2 +1,18 @@
 class Volunteer < ActiveRecord::Base
+
+  validates_presence_of :first_name, :last_name, :email
+  validates_format_of :email, :with => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+
+  require 'digest/md5'
+  
+  before_save :make_token
+
+ def make_token
+    self.token = Digest::MD5.hexdigest("#{self.email}-#{Time.now.to_i}")
+  end
+  
+  def unsubscribe_link
+    "http://www.ericlarsonmd.com/volunteers/show/#{self.token}"
+  end
+    
 end
