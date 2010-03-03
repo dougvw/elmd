9127cbe2c792ea3b5ad25ae80676bd76af1e1062
D. Vander Woude <dougvw@gmail.com>
2010-03-03 16:10:38 -0500

    deploy changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,26 +1,29 @@
 # set :application, "example"
 # set :repository, "git@example.com:example.git"
 # set :server, "example.com"
-
-set :domain, "ericlarson2010.com"   #the one you ssh into
-set :user, "dougv"            #the user you created when setting up the domain (has to have shell access)
+#set :scm, :git
 set :application, "elmd"          #the name of the folder you chose when setting up the domain
+set :repository, "ssh://dlv@bandit.homelinux.net:8008/home/git/elmd.git"
+set :branch, "master"
+set :domain, "ericlarson2010.com"   #the one you ssh into
+#set :user, "dougv"            #the user you created when setting up the domain (has to have shell access)
+
 set :applicationdir, "/home/#{user}/#{application}"  # The standard Dreamhost setup
  
-set :repository, "ssh://dlv@bandit.homelinux.net:8008/home/git/elmd.git"
-set :scm, :git
+#set :repository, "ssh://dlv@bandit.homelinux.net:8008/home/git/elmd.git"
+#set :scm, :git
  
 # deploy config
-set :deploy_to, applicationdir       # Where on the server your app will be deployed
-set :deploy_via, :remote_cache
+#set :deploy_to, applicationdir       # Where on the server your app will be deployed
+#set :deploy_via, :remote_cache
  
 # additional settings
-default_run_options[:pty] = true  # Forgo errors when deploying from windows
+#default_run_options[:pty] = true  # Forgo errors when deploying from windows
 #ssh_options[:keys] = %w(/Path/To/id_rsa)            # If you are using ssh_keys
-set :chmod755, "app config db lib public vendor script script/* public/disp*"
-set :use_sudo, false
+#set :chmod755, "app config db lib public vendor script script/* public/disp*"
+#set :use_sudo, false
 
-set :branch, "master"
+#set :branch, "master"
  
 role :app, domain
 role :web, domain
